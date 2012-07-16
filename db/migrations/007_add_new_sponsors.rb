1e3805282bc8d13dd3a9656ab44c018d4e1032d2
Michel Blanc <mb@mbnet.fr>
2012-07-16 13:53:40 +0200

    Removed some sponsors
    First sponsors were added twice

--- db/migrations/007_add_new_sponsors.rb
+++ db/migrations/007_add_new_sponsors.rb
@@ -7,33 +7,6 @@
 Sequel.migration do
   change do
     transaction do
-      self[:sponsors].insert(:name => 'Communauté de Communes de Chamousset en Lyonnais', 
-        :logo => 'images/sponsors/cccl.jpg', :url => 'http://www.chamousset-en-lyonnais.com',
-        :description => 'Communauté de Communes de Chamousset en Lyonnais', :display => true)
-
-      self[:sponsors].insert(:name => 'Collège Saint Laurent de Chamousset', 
-        :logo => 'images/sponsors/col_slc.jpg', :url => 'http://www.college-st-laurent.fr',
-        :description => 'Communauté de Communes de Chamousset en Lyonnais', :display => true)
-
-      self[:sponsors].insert(:name => 'Handisport Val d\'Ozon', 
-        :logo => 'images/sponsors/handi_val_ozon.jpg', :url => '#',
-        :description => 'Handisport Val d\'Ozon', :display => true)
-
-      self[:sponsors].insert(:name => 'O"rka', 
-        :logo => 'images/sponsors/orka.jpg', :url => 'http://www.orka-cycles.com/',
-        :description => 'O"rka', :display => true)
-
-      self[:sponsors].insert(:name => 'Département du Rhône', 
-        :logo => 'images/sponsors/rhone.jpg', :url => 'http://www.rhone.fr', :description => 'Département du Rhône', :display => true)
-
-      self[:sponsors].insert(:name => 'Commune de Saint Laurent de Chamousset', 
-        :logo => 'images/sponsors/slc.jpg', :url => 'http://chamousset-en-lyonnais.com/index.php?page=st-laurent',
-        :description => 'Commune de Saint Laurent de Chamousset', :display => true)
-
-      self[:sponsors].insert(:name => 'Union Nationale du Sport Scolaire', 
-        :logo => 'images/sponsors/unss.jpg', :url => 'http://www.unss.org/',
-        :description => 'Union Nationale du Sport Scolaire', :display => true)
-
       self[:sponsors].insert(:name => 'Asterion',
         :logo => 'images/sponsors/asterion.jpg', :url => 'http://www.asterion-wheels.com',
         :description => 'Roues de vélo hautes performances VTT et route', :display => true)

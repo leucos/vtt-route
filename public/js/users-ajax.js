56e6c717ac634a8a035b071c369b4fbc904d983b
Michel Blanc <mb@mbnet.fr>
2012-09-02 16:16:46 +0200

    Adds ajax js script for user search
    User searching ajax function skeleton

---
+++ public/js/users-ajax.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('#search').focusout(function() {
+    $('#propositions').hide();
+  });  
+  $('#search').keyup(function() {
+    console.log("got key up");
+    var pattern = $('#search').val();
+    if (pattern.length > 3 ) {
+      $.ajax({
+        url: "/backoffice/users/" + $("#search").val() + ".json",
+        success: function(result){
+          $('#propositions').show(function() {
+            var buf = "";
+            for (var i=0; i<result.length; i++) {
+              console.log(result[i]["values"]["name"]);
+              buf += "<p><a href=#>" + result[i]["values"]["surname"] + " " + result[i]["values"]["name"] + "</a></p>";
+            }
+            $(this).html(buf);
+          });
+        }
+      });
+    }
+  });
+});
+
+

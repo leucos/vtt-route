b8874d4468d1ecc4d66ac335e1a7a179b9785063
Michel Blanc <mb@mbnet.fr>
2012-09-02 16:19:35 +0200

    Fixes sidekiq lib name
    Lib name for mailhandling in sidekiq was renamed some commits ago, but
    not changed in daemon wrapper.

--- bin/daemon_sidekiq-workers.rb
+++ bin/daemon_sidekiq-workers.rb
@@ -6,7 +6,7 @@ path=`bundle exec which sidekiq`.chomp
 
 Daemons.run(path, 
             :app_name => 'sidekiq-workers',
-            :ARGV => [ ARGV[0], '--', '-v', '-q', 'high,5', 'default', '-r', "#{Dir.pwd}/lib/mail_worker.rb" ],
+            :ARGV => [ ARGV[0], '--', '-v', '-q', 'high,5', 'default', '-r', "#{Dir.pwd}/lib/mail_utils.rb" ],
             :log_dir => './log/',
             :log_output => true,
             :dir_mode => :normal,
